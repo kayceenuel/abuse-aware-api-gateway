acb518cefe6859d74783f40a949b591fddc01360
Kene-dili-chukwu <heykayceee@gmail.com>
2026-07-23 16:55:04 +0000

    add loginHandler enforcing POST-only requests

---
+++ handlers/handler.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+}
