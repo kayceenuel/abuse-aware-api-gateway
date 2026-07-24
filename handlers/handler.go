e0f21a4855112bba1fc274bc0fe184ebf6464293
Kene-dili-chukwu <heykayceee@gmail.com>
2026-07-24 16:30:10 +0000

    modified handler routes

--- handlers/handler.go
+++ handlers/handler.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost { // check if the request method is POST
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
@@ -10,14 +10,14 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close() // close the request body when the function returns
 }
 
-func searchHandler(w http.ResponseWriter, r *http.Request) {
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet { // check if the request method is GET
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 }
 
-func purchaseHandler(w http.ResponseWriter, r *http.Request) {
+func PurchaseHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
