8fdfa95f1795e51fa80d4305d19115a5ed80d5de
Kene-dili-chukwu <heykayceee@gmail.com>
2026-07-30 07:25:52 +0000

    feat: updated handler to forward request using proxy

--- handlers/handler.go
+++ handlers/handler.go
@@ -24,10 +24,15 @@ func SearchHandler(proxy http.Handler) http.HandlerFunc {
 	}
 }
 
-func PurchaseHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
+func PurchaseHandler(proxy http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			return
+		}
+		defer r.Body.Close()
+		// Call the proxy to forward the request if the method is correct.
+		proxy.ServeHTTP(w, r)
 	}
-	defer r.Body.Close()
+
 }
