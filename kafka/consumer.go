ccf63cbd5145f2f7099d049ffd15a2497fe71839
Kene-dili-chukwu <heykayceee@gmail.com>
2026-08-12 10:29:20 +0000

    add Kafka consumer with message deserialization

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -1,1 +1,33 @@
 package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+func Start(brokerAddress, topic string) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{brokerAddress},
+		GroupID: "gateway_consumer",
+		Topic:   topic,
+	})
+	defer r.Close()
+
+	for {
+		m, err := r.ReadMessage(context.Background())
+		if err != nil {
+			log.Printf("error reading message: %v", err)
+			continue
+		}
+
+		var event RequestEvent
+		if err := json.Unmarshal(m.Value, &event); err != nil {
+			log.Printf("failed to deserialize event: %v", err)
+			continue
+		}
+		// TODO: pass event to risk scorer
+	}
+}
