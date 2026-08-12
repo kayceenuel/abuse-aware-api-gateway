b406d2ccfff498b81c5424f91b54de6814be8e61
Kene-dili-chukwu <heykayceee@gmail.com>
2026-08-12 11:52:02 +0000

    update: add data structure for risk_scorer

--- kafka/risk_scorer.go
+++ kafka/risk_scorer.go
@@ -1,1 +1,9 @@
 package kafka
+
+import "github.com/redis/go-redis/v9"
+
+type RiskScorer struct {
+	client            *redis.Client // to read & write counters
+	stuffingThreshold int // how many unique username from one IP before it's supicious
+	scrapingThreshold int // how many /search request before it's supicious
+}
