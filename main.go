cc5cb6a40d49aaaf4563392d0593bc99156ab339
Kene-dili-chukwu <heykayceee@gmail.com>
2026-08-17 03:48:42 +0000

    feat(main): pass Kafka producer to route handlers

--- main.go
+++ main.go
@@ -30,17 +30,19 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// create risk scorer
 	scorer := kafka.NewRiskScorer(redisClient, 10, 50)
 
+	// create kafka producer
 	producer := kafka.NewProducer("localhost:9092", "gateway_events")
 	defer producer.Close()
 
 	// start consumer in background
 	go kafka.Start("localhost:9092", "gateway_events", scorer)
 	// handle the routes: /login, /search, /purchase
-	http.Handle("/login", handlers.LoginHandler(proxyHandler, rateLimiter))
-	http.Handle("/search", handlers.SearchHandler(proxyHandler, rateLimiter))
-	http.Handle("/purchase", handlers.PurchaseHandler(proxyHandler, rateLimiter))
+	http.Handle("/login", handlers.LoginHandler(proxyHandler, rateLimiter, producer))
+	http.Handle("/search", handlers.SearchHandler(proxyHandler, rateLimiter, producer))
+	http.Handle("/purchase", handlers.PurchaseHandler(proxyHandler, rateLimiter, producer))
 
 	// set up the HTTP server
 	fmt.Println("Server is running on http://localhost:2121")
