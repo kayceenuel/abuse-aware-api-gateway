6997205708db15bb7480c2f9dac551c85762530a
Kene-dili-chukwu <heykayceee@gmail.com>
2026-08-19 17:47:49 +0000

    added Credential stuffing and scraping  simulation func

---
+++ scripts/replay.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func simulateCredentialStuffing(client *http.Client) {
+	for i := 1; i <= 20; i++ {
+		url := "http://localhost:2121/login"
+		req, err := http.NewRequest("POST", url, nil)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			return
+		}
+		req.Header.Add("X-API-Key", "testKey123")
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("request failed:  %v\n", err)
+			return
+		}
+		fmt.Printf("[CREDENTIAL STUFFING] attempt %d — username: user%d → %s\n", i, i, res.Status)
+		res.Body.Close()
+	}
+}
+
+func simulateScraping(client *http.Client) {
+
+}
+
+func main() {
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	simulateCredentialStuffing(client)
+	simulateScraping(client)
+
+}
